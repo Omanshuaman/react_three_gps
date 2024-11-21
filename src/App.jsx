dcc2a8aad7570421766f8ea72d842f9ecdd841c6
Omanshuaman <omanshuaman@gmail.com>
2024-11-21 17:37:44 +0530

    mesh added inside map.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "maplibre-gl/dist/maplibre-gl.css";
+import Map from "react-map-gl/maplibre";
+import { Canvas } from "react-three-map/maplibre";
+import React, { useRef, useState } from "react";
+import { Environment } from "@react-three/drei";
 
+import { useFrame } from "@react-three/fiber";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Map
+          antialias
+          initialViewState={{
+            latitude: 51,
+            longitude: 0,
+            zoom: 13,
+            pitch: 60,
+          }}
+          mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json">
+          <Canvas latitude={51} longitude={0}>
+            <Environment preset="park" />
 
-export default App
+            <object3D scale={500}>
+              <Box position={[-1.2, 0, 0]} />
+              <Box position={[1.2, 0, 0]} />
+            </object3D>
+            <axesHelper args={[5000000]} />
+          </Canvas>
+        </Map>
+      </div>
+    </>
+  );
+}
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+export default App;
