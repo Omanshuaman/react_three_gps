ee74ba9d8e9fb9190a9423fabdafbd71b94c4698
Omanshuaman <omanshuaman@gmail.com>
2024-12-07 20:55:04 +0530

    tailwind added

--- vite/earth-main/src/index.jsx
+++ vite/earth-main/src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import React from "react";
-
+import "./index.css";
 const app = document.getElementById("app");
 ReactDOM.render(<App />, app);
